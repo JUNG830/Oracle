132790abc3ff7d11ca25f812c6f12869d8ba9843
NewHJ <>
2022-08-25 17:15:18 +0900

    0825

--- sql/연습문제.sql
+++ sql/연습문제.sql
@@ -72,3 +72,46 @@ WHERE TO_CHAR(SYSDATE - HIREDATE) >= 13870 ;
 -- 3. **오늘 날짜에서 년도만 추출**
 SELECT EXTRACT(YEAR FROM SYSDATE) 연도
 FROM DUAL;
+
+-- 19. 이름이 S자로 시작하고, 마지막 글자가H인 사원의 모든 정보 출력
+SELECT * 
+FROM EMP
+WHERE ENAME LIKE 'S%' AND ENAME LIKE '%H' ;
+-- 20. 처음의 글자는 관계없고 두번째 글자가 A인 사원 모든 정보 출력
+SELECT *
+FROM EMP
+WHERE ENAME LIKE '_A%' ; 
+-- 21. 급여가 1600 이상이고 부서가 30번인 사원 중 직책이 MANAGER 인 사원의 모든 정보 출력
+SELECT *
+FROM EMP
+WHERE (SAL >= 1600 AND DEPTNO = 30) AND JOB = 'MANAGER' ;
+-- 22. 사원의 모든 정보를 부서번호에 대한 내림차순, 이름에 대한 오름차순, 급여에 대한 내림차순 정렬
+SELECT *
+FROM EMP
+ORDER BY DEPTNO DESC, ENAME, SAL DESC;
+-- 23. 사원번호가 7654와 7782 사이 이외의 모든 정보 출력
+SELECT *
+FROM EMP
+WHERE EMPNO NOT BETWEEN 7654 AND 7782;
+-- 24. 사원의 모든 정보를 부서 번호에 대해 오름차순 정렬 후 급여가 많은 사원부터 출력
+SELECT *
+FROM EMP
+ORDER BY DEPTNO, SAL DESC ;
+-- 25. 직책이 MANAGER 가 아니고, 부서가 20이 아닌 사원의 모든 정보 출력
+SELECT *
+FROM EMP
+WHERE JOB != 'MANAGER' AND DEPTNO != 20;
+-- 26. 1981/04/01 이후 입사하고, 부서가 30인 사원 정보 
+SELECT *
+FROM EMP
+WHERE HIREDATE > '81/04/01' AND DEPTNO = 30
+ORDER BY SAL DESC;
+-- 27. 10번 부서에 모든 사원에게 급여의 13%를 보너스로 지급, 10번 부서의 사원 이름, 급여, 보너스 총 금액, 부서번호 출력
+SELECT  ENAME, SAL, FLOOR(SAL*1.13) 보너스포함, DEPTNO 
+FROM EMP
+WHERE DEPTNO = 10; 
+-- 28. 모든 사원에 대해서 입사일부터 90일이 지난 후의 날짜 계산, 이름, 입사일, 입사 후 90일, 급여(90일치 급여??) 
+SELECT ENAME, HIREDATE, HIREDATE + 90 "90일후" , SAL*3 "총급여"
+FROM EMP
+ORDER BY 총급여 DESC;
+
